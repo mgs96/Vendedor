b90d670f773899495578bfa344390bcb351f2f15
Alexander Taborda Acosta <aletab93@gmail.com>
2017-05-15 17:35:21 -0500

    joaaa, triste mk

--- app/src/main/java/com/movil/mauricio_taborda/vendedor/login.java
+++ app/src/main/java/com/movil/mauricio_taborda/vendedor/login.java
@@ -1,57 +1,58 @@
 package com.movil.mauricio_taborda.vendedor;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    FirebaseUser user;
-    EditText email;
-    EditText password;
+
+    private static final int RC_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
-        email = (EditText) findViewById(R.id.etLogin);
-        password = (EditText) findViewById(R.id.etPass);
+
+        if(mAuth.getCurrentUser() != null){
+            //user signed
+        }else {
+            startActivityForResult(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setProviders(AuthUI.EMAIL_PROVIDER, AuthUI.GOOGLE_PROVIDER)
+                    .build(), RC_SIGN_IN);
+        }
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                Log.d("AUTH", mAuth.getCurrentUser().getEmail());
+            }
+        }
     }
 
-    public void login(View view) {
-        mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+    public void logout(View view) {
+        AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    user = mAuth.getCurrentUser();
-                    Toast.makeText(getApplicationContext(), user.getUid(), Toast.LENGTH_SHORT).show();
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Toast.makeText(login.this, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show();
-                }
-
+            public void onComplete(@NonNull Task<Void> task) {
+                Log.d("AUTH", "LOGOUT");
             }
         });
+
     }
 }
